5f2567ea4bd29203d24630a9cd5a6d3ac2bdd956
Nicolas Ollier <nicolas@lepetitmartin.com>
2024-04-04 18:50:26 +0200

    setup click movement

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { scaleFactor } from './constants'
 import { k } from './kaboom/kaboomCtx'
-import { displayDialogue } from './utils'
+import { displayDialogue, setCamScale } from './utils'
 
 k.loadSprite("spritesheet", "./assets/spritesheet.png", {
   sliceX: 39,
@@ -81,6 +81,9 @@ k.scene("main", async () => {
     }
   }
 
+  setCamScale(k);
+  k.onResize(() => setCamScale(k));
+
   k.onUpdate(() => {
     k.camPos(player.worldPos().x, player.worldPos().y - 100);
   });
@@ -90,8 +93,61 @@ k.scene("main", async () => {
 
     const worldMousePos = k.toWorld(k.mousePos());
     player.moveTo(worldMousePos, player.speed);
+
+    const mouseAngle = player.pos.angle(worldMousePos);
+
+    const lowerBound = 50;
+    const upperBound = 125;
+
+    if (
+      mouseAngle > lowerBound &&
+      mouseAngle < upperBound &&
+      player.curAnim() !== "walk-up"
+    ) {
+      player.play("walk-up");
+      player.direction = "up";
+      return;
+    }
+
+    if (
+      mouseAngle < -lowerBound &&
+      mouseAngle > -upperBound &&
+      player.curAnim() !== "walk-down"
+    ) {
+      player.play("walk-down");
+      player.direction = "down";
+      return;
+    }
+
+    if (Math.abs(mouseAngle) > upperBound) {
+      player.flipX = false;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "right";
+      return;
+    }
+
+    if (Math.abs(mouseAngle) < lowerBound) {
+      player.flipX = true;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "left";
+      return;
+    }
   });
-  //----------- //
+
+  function stopAnims() {
+    if (player.direction === "down") {
+      player.play("idle-down");
+      return;
+    }
+    if (player.direction === "up") {
+      player.play("idle-up");
+      return;
+    }
+
+    player.play("idle-side");
+  }
+
+  k.onMouseRelease(stopAnims);
 });
 
 k.go("main");
