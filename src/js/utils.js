1d1bbcf1efa0b56670c12cc6e573c397d7aee73a
Nicolas Ollier <nicolas@lepetitmartin.com>
2024-04-04 17:38:34 +0200

    handle close dialogue

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,32 @@
 export function displayDialogue(text, onDisplayEnd) {
-  const dialogue = document.getElementById("textbox-container");
+  const dialogueUI = document.getElementById("textbox-container")
+  const dialogue = document.getElementById("dialogue")
+  const closeBtn = document.getElementById("close-btn")
+
+  dialogueUI.style.display = "block"
+
+  let i = 0
+  let currentText = ""
+
+  const interval = setInterval(() => {
+    if (i < text.length) {
+      currentText += text[i]
+      dialogue.innerHTML = currentText
+      i++
+
+      return
+    }
+
+    clearInterval(interval)
+  }, 5)
+
+  function handleClose() {
+    onDisplayEnd()
+    dialogueUI.style.display = "none"
+    dialogue.innerHTML = ""
+    clearInterval(interval)
+    closeBtn.removeEventListener("click", handleClose)
+  }
+
+  closeBtn.addEventListener("click", handleClose)
 }
