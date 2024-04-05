7b37fe61ce89b16cd9b4131dad57ba8d90abaa39
Nicolas Ollier <nicolas@lepetitmartin.com>
2024-04-05 14:49:10 +0200

    improves links

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,14 +3,14 @@ export const scaleFactor = 4;
 export const dialogueData = {
   pc: `This is my PC, my main tool for coding. Lately, I've been immersed in JavaScript/TypeScript projects.
     I've built a few things with these languages. Honestly, I just love programming, regardless of the language.
-    Check out my <a href="https://github.com/nicolasollier" target="_blank">Github</a> to see what I've been up to!`,
+    Check out my <a href="https://github.com/nicolasollier" target="_blank">👉 Github</a> to see what I've been up to!`,
   "cs-degree": `Here's my Openclassrooms degree! I'm really proud of it, so much so that I've pinned it here. Guess this officially makes me a front-end developer, huh?`,
   "sofa-table": `This is my go-to relaxation spot, my sofa! I unwind here, diving into PS5 adventures. Currently, I'm engrossed in Baldur's Gate 3. Such an epic game!`,
   tv: `Meet my TV, the core of my movie and series binge-watching sessions. Right now, I'm all about Arcane. It's just brilliant!`,
   bed: `This is where the magic happens - my bed. It's where I dream up my best ideas. Maybe it's time to keep a notebook handy...`,
-  resume: `And this is my workspace, complete with my resume. Have a look at my <a href="https://linkedin.com/in/nicolasollier2" target="_blank">LinkedIn</a>.
+  resume: `And this is my workspace, complete with my resume. Have a look at my <a href="https://linkedin.com/in/nicolasollier2" target="_blank">👉 LinkedIn</a>.
   Got any exciting job leads? Reach out at nicolas.f.ollier@gmail.com!`,
-  projects: `Here's my resume, in print. I'm hesitant to add more to it; it's just so aesthetically pleasing as is. But you can see more about my projects <a href="https://shorturl.at/uFNQ2" target="_blank">here</a>!`,
+  projects: `Here's my resume, in print. I'm hesitant to add more to it; it's just so aesthetically pleasing as is. But you can see more about my projects <a href="https://shorturl.at/uFNQ2" target="_blank">👉 here</a>!`,
   library: `My shelves are brimming with programming books. Confession: I've probably fully read just one. Am I the only one who buys tech books more for the collection than the reading?`,
   exit: `Step outside? Why would I? Everything I need is right here, in my personal haven.`,
 };
