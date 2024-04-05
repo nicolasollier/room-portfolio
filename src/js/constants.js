202b241a1a26f4c2a2ee932dba69c3252277aa76
Nicolas Ollier <nicolas@lepetitmartin.com>
2024-04-05 14:06:08 +0200

    few fixed

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,23 +1,16 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  pc: `This is my PC, my main tool for coding. Lately, I've been immersed in JavaScript/TypeScript projects.
+    I've built a few things with these languages. Honestly, I just love programming, regardless of the language.
+    Check out my <a href="https://github.com/nicolasollier" target="_blank">Github</a> to see what I've been up to!`,
+  "cs-degree": `Here's my Openclassrooms degree! I'm really proud of it, so much so that I've pinned it here. Guess this officially makes me a front-end developer, huh?`,
+  "sofa-table": `This is my go-to relaxation spot, my sofa! I unwind here, diving into PS5 adventures. Currently, I'm engrossed in Baldur's Gate 3. Such an epic game!`,
+  tv: `Meet my TV, the core of my movie and series binge-watching sessions. Right now, I'm all about Arcane. It's just brilliant!`,
+  bed: `This is where the magic happens - my bed. It's where I dream up my best ideas. Maybe it's time to keep a notebook handy...`,
+  resume: `And this is my workspace, complete with my resume. Have a look at my <a href="https://linkedin.com/in/nicolasollier2" target="_blank">LinkedIn</a>.
+  Got any exciting job leads? Reach out at nicolas.f.ollier@gmail.com!`,
+  projects: `Here's my resume, in print. I'm hesitant to add more to it; it's just so aesthetically pleasing as is. But you can see more about my projects <a href="https://shorturl.at/uFNQ2" target="_blank">here</a>!`,
+  library: `My shelves are brimming with programming books. Confession: I've probably fully read just one. Am I the only one who buys tech books more for the collection than the reading?`,
+  exit: `Step outside? Why would I? Everything I need is right here, in my personal haven.`,
 };
